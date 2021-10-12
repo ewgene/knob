b3b03646876b931e17bce1a6858279655704f7f8
JumpimgJack <ewgene.shure@gmail.com>
2021-10-12 14:49:11 +0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div class="knob">
+      <slider />
+    </div>
+  </div>
+</template>
+
+<script>
+import slider from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    slider
+  },
+  data () {
+    return {
+      range: {
+        high: null,
+        low: null
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 128px;
+  height: 128px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #090909;
+  margin: 60px auto;
+}
+.knob {
+  width: 128px;
+  height: 128px;
+  background-image: url('./assets/knob-bg.png');
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <knob
+      radius="63"
+      width="8"
+      showTooltip="false"
+      pathColor="#090909"
+      rangeColor="transparent" />
+  </div>
+</template>
+
+<script>
+import knob from 'vue-round-slider'
+export default {
+  components: {
+    knob
+  },
+  name: 'slider',
+  data () {
+    return {
+      max: '100',
+      min: '0'
+    }
+  },
+  mounted: function () {
+    let target = document.querySelector('.rs-move')
+    target.addClass = 'knob'
+    console.log(target)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
